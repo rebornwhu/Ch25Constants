b312fe3833aa01725d5a8e44fdc76f6126985d1a
xiaolu <rebornwhu@gmail.com>
2015-03-24 22:11:04 -0700

    First commit

---
+++ Ch25Constants/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ch25Constants
+//
+//  Created by Xiao Lu on 3/24/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
